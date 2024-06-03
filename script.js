ec281f3d4064685c1d22928533772d5c1479fa56
Raissa Faustino Moreira <110838583+faustaocareca@users.noreply.github.com>
2024-06-03 11:42:33 -0300

    aula3parcial

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal= document.querySelector(".caixa-Principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaresultado= document.querySelector(".caixa-resultado");
+const textoresultado= document.querySelector("texto-resltado");
